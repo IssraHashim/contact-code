411d362de588aac8664c2ce7406854cb881904c6
Issra Hashim <issrahashim@hotmail.fr>
2022-02-04 13:20:35 +0000

    added front-end with option to select locations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+
 
 function App() {
+  const data = [
+    {
+      "name": "Homer Simpson",
+      "location": "Springfield",
+      "date_of_birth": "1956-05-12"
+    },
+    {
+      "name": "Frank Reynolds",
+      "location": "Philidelphia",
+      "date_of_birth": "1944-11-17"
+    },
+    {
+      "name": "Diane Nguyen",
+      "location": "Los Angeles",
+      "date_of_birth": "1980-03-19"
+    },
+    {
+      "name": "Krusty the Clown",
+      "location": "SpringField",
+      "date_of_birth": "1957-10-29"
+    }
+  ]
+
+  const [location, setLocation] = useState([])
+  const [display, setDisplay] = useState([])
+  const allLocations = []
+
+
+
+  useEffect(() => {
+    for (let i=0; i < data.length; i++) {
+      if (!allLocations.includes(data[i].location.toLocaleLowerCase())) {
+        allLocations.push(data[i].location.toLocaleLowerCase())
+      }
+    }
+    // data.forEach(content => {
+    //   console.log(allLocations.includes(content.location))
+    //   console.log(allLocations)
+    //   if (!allLocations.includes(content.location)) {
+    //     allLocations.push(content.location)
+    //   }
+    // })
+    setLocation(allLocations)
+  }, [])
+
+
+  const handleClick = (event) => {
+    const thisLocation = event.target.value
+    const results = []
+    data.forEach(content => {
+      if (content.location.toLocaleLowerCase() === thisLocation) {
+        results.push(content.name)
+      }
+    })
+    setDisplay(results)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section>          
+      <div style={{ display: 'flex' , justifyContent: 'space-evenly', marginTop: '50px', marginBottom: '100px' }}>
+        {location.map(place =>{
+          return (
+              <button key={place} value={place} onClick={handleClick} style={{ padding: '10px', textTransform: 'capitalize'}}>{place}</button>
+          )
+        })}
+      </div>
+      {display &&
+      display.map(result => {
+        return (
+          <>
+          <h3 key={result} style={{ textAlign: 'center' }}>{result}</h3>
+          </>
+        )
+      })
+        }
+    </section>
+  )
 }
 
 export default App;
